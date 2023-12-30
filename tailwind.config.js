fdf593762db23db1f76926d58c0856a3620f32cd
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-30 18:02:43 +0300

    Add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        'primary-bright-red': 'hsl(12, 88%, 59%)',
+        'primary-dark-blue': 'hsl(228, 39%, 23%)',
+        // neutral
+        'neutral-grayish-blue': 'hsl(227, 12%, 61%)',
+        'neutral-dark-blue': 'hsl(233, 12%, 13%)',
+        'neutral-pale-red': 'hsl(13, 100%, 96%)',
+        'neutral-light-gray': 'hsl(0, 0%, 98%)',
+      },
+    },
+    screens: {
+      'sm': '375px',
+      'md': '768px',
+      'lg': '1440px',
+    },
   },
   plugins: [],
 }
