6a0973bf947d2e0dd49d697e4f6888310e676687
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-31 12:20:11 +0300

    fix padding for home tablet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="bg-[url('/src/assets/images/bg-tablet-pattern.svg')] h-screen w-full bg-no-repeat bg-auto bg-right-bottom top-0 left-0"
+      className="bg-[url('/src/assets/images/bg-tablet-pattern.svg')] h-screen w-full bg-no-repeat bg-auto bg-[right_-12rem_bottom_4rem] top-0 left-0"
     >
       <Navbar />
-      <div className="flex flex-col lg:flex-row lg:justify-evenly lg:px-40 lg:py-12">
+      <div className="flex flex-col lg:flex-row lg:justify-evenly lg:px-40 lg:py-10">
         <div className="flex flex-col justify-center w-3/5 gap-8 text-left">
           <h1 className="font-bold text-5xl text-primary-dark-blue"> Bring everyone together to build better products.</h1>
           <p className="text-neutral-grayish-blue font-light text-md pr-36">
@@ -20,7 +20,7 @@ const Home = () => {
           </button>
         </div>
         <div>
-          <img src="/src/assets/images/illustration-intro.svg" alt="" />
+          <img src="/src/assets/images/illustration-intro.svg" alt="Illustraion image" className="ml-12" />
         </div>
       </div>
     </div>
