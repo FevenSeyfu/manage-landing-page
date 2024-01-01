cfe31c6ff94106f5b4fa174c3f37c3561530682b
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-01 19:59:39 +0300

    compelete footer for desktop view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           Get Started
         </button>
       </div>
-      <div className="bg-primary-dark-blue text-white flex flex-col gap-8 items-center px-2 py-6">
+      <div className="bg-primary-dark-blue text-white flex flex-col gap-8 items-start px-2 py-6 md:hidden">
         <div>
           <input
             type="text"
@@ -65,50 +65,69 @@ const Footer = () => {
             </div>
           </ul>
         </div>
-        <div id="social" >
+        <div id="social">
           <ul className="flex flex-row items-center gap-8">
             <li>
               <a href="#">
                 <img
                   src="src/assets/images/icon-facebook.svg"
                   alt="youtube icon"
-                  className="h-8"
+                  className="h-6"
                 />
               </a>
             </li>
             <li>
               {/* youtube */}
               <a href="#">
-                <img src="src/assets/images/icon-youtube.svg" alt="social icon" className="h-8" />
+                <img
+                  src="src/assets/images/icon-youtube.svg"
+                  alt="social icon"
+                  className="h-6"
+                />
               </a>
             </li>
             <li>
               {/* twitter */}
               <a href="#">
-                <img src="src/assets/images/icon-twitter.svg" alt="social icon" className="h-8" />
+                <img
+                  src="src/assets/images/icon-twitter.svg"
+                  alt="social icon"
+                  className="h-6"
+                />
               </a>
             </li>
             <li>
               {/* pinterest */}
               <a href="#">
-                <img src="src/assets/images/icon-pinterest.svg" alt="social icon" className="h-8" />
+                <img
+                  src="src/assets/images/icon-pinterest.svg"
+                  alt="social icon"
+                  className="h-6"
+                />
               </a>
             </li>
             <li>
               {/* instagram */}
               <a href="#">
-                <img src="src/assets/images/icon-instagram.svg" alt="social icon" className="h-8" />
+                <img
+                  src="src/assets/images/icon-instagram.svg"
+                  alt="social icon"
+                  className="h-6"
+                />
               </a>
             </li>
           </ul>
         </div>
-        <div id="logo" >
+        <div id="logo">
           <img src="src/assets/images/logo-white.svg" alt="Logo" />
         </div>
-        <div id="copyright" className="flex flex-col items-center text-sm text-primary-grayish-blue">
+        <div
+          id="copyright"
+          className="flex flex-col items-center text-sm text-primary-grayish-blue"
+        >
           <p>Copyright 2020. All Rights Reserved</p>
           <div class="attribution px-12 text-center">
-            Challenge by 
+            Challenge by
             <a
               href="https://www.frontendmentor.io?ref=challenge"
               target="_blank"
@@ -116,7 +135,154 @@ const Footer = () => {
             >
               Frontend Mentor
             </a>
-            . Coded by <a href="https://www.frontendmentor.io/profile/FevenSeyfu" className="hover:underline hover:text-primary-bright-red "> Feven Seyfu</a>.
+            . Coded by{" "}
+            <a
+              href="https://www.frontendmentor.io/profile/FevenSeyfu"
+              className="hover:underline hover:text-primary-bright-red "
+            >
+              {" "}
+              Feven Seyfu
+            </a>
+            .
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-primary-dark-blue text-white md:flex flex-row justify-evenly p-12 items-center">
+        <div className="flex flex-col items-center gap-12">
+          <div id="logo">
+            <img src="src/assets/images/logo-white.svg" alt="Logo" />
+          </div>
+          <div id="social">
+            <ul className="flex flex-row items-center gap-2">
+              <li>
+                <a href="#">
+                  <img
+                    src="src/assets/images/icon-facebook.svg"
+                    alt="youtube icon"
+                    className="h-6"
+                  />
+                </a>
+              </li>
+              <li>
+                {/* youtube */}
+                <a href="#">
+                  <img
+                    src="src/assets/images/icon-youtube.svg"
+                    alt="social icon"
+                    className="h-6"
+                  />
+                </a>
+              </li>
+              <li>
+                {/* twitter */}
+                <a href="#">
+                  <img
+                    src="src/assets/images/icon-twitter.svg"
+                    alt="social icon"
+                    className="h-6"
+                  />
+                </a>
+              </li>
+              <li>
+                {/* pinterest */}
+                <a href="#">
+                  <img
+                    src="src/assets/images/icon-pinterest.svg"
+                    alt="social icon"
+                    className="h-6"
+                  />
+                </a>
+              </li>
+              <li>
+                {/* instagram */}
+                <a href="#">
+                  <img
+                    src="src/assets/images/icon-instagram.svg"
+                    alt="social icon"
+                    className="h-6"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div id="site-map">
+          <ul className="flex flex-row gap-32 text-lg">
+            <div className="flex flex-col gap-2">
+              <li>
+                <a href="#" className="hover:text-primary-bright-red ">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-primary-bright-red ">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-primary-bright-red ">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-primary-bright-red ">
+                  About Us
+                </a>
+              </li>
+            </div>
+            <div className="flex flex-col gap-2">
+              <li>
+                <a href="#" className="hover:text-primary-bright-red ">
+                  Careers
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-primary-bright-red ">
+                  Community
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-primary-bright-red ">
+                  Privacy Policy
+                </a>
+              </li>
+            </div>
+          </ul>
+        </div>
+        <div className="flex flex-col items-center gap-6">
+          <div>
+            <input
+              type="text"
+              placeholder="Updates in your inbox…"
+              className="p-2 rounded-full mr-2 pr-12"
+            />
+            <button className="bg-primary-bright-red text-white rounded-full px-6 py-2">
+              Go
+            </button>
+          </div>
+          <div
+            id="copyright"
+            className="flex flex-col items-center text-sm text-primary-grayish-blue"
+          >
+            <p>Copyright 2020. All Rights Reserved</p>
+            <div class="attribution px-12 text-center">
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+                className="hover:underline hover:text-primary-bright-red "
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a
+                href="https://www.frontendmentor.io/profile/FevenSeyfu"
+                className="hover:underline hover:text-primary-bright-red "
+              >
+                Feven Seyfu
+              </a>
+              .
+            </div>
           </div>
         </div>
       </div>
