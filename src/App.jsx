7a3eae0bc5962c5bd604ad25125016b01f418a4c
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-01 20:30:54 +0300

    Update tablet position

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Home />
       <div
-      className="bg-[url('/src/assets/images/bg-tablet-pattern.svg')] h-full w-full bg-no-repeat mb-18 lg:mb-1 lg:bg-auto lg:bg-[left_-30rem_top_12rem]">
+      className="lg:bg-[url('/src/assets/images/bg-tablet-pattern.svg')] h-full w-full bg-no-repeat mb-18 lg:mb-1 lg:bg-auto lg:bg-[left_-30rem_top_12rem]">
         <About />
         <Testimonials />
       </div>
