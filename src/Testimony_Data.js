64edfcf243893dc1834cc38564eeefdf2dec06cf
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-02 16:35:25 +0300

    update relative link

--- src/Testimony_Data.js
+++ src/Testimony_Data.js
@@ -3,14 +3,14 @@ const slides = [
     {
       id: 0,
       user_name: "Anisha Li",
-      profileImg: "../assets/images/avatar-anisha.png",
+      profileImg: "./assets/images/avatar-anisha.png",
       testimony:
         '"Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated."',
     },
     {
       id: 1,
       user_name: "Ali Bravo",
-      profileImg: "../assets/images/avatar-ali.png",
+      profileImg: "./assets/images/avatar-ali.png",
       testimony:
         "“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”",
     },
@@ -24,7 +24,7 @@ const slides = [
     {
       id: 3,
       user_name: "Shanai Gough",
-      profileImg: "../assets/images/avatar-shanai.png",
+      profileImg: "./assets/images/avatar-shanai.png",
       testimony:
         "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”",
     },
